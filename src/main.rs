f161c48ccc2ab0039bbf36bf848ce7aaf06b8506
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2021-02-16 20:49:33 -0800

    Replaces lots and assets commands with status.

--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,93 @@ extern crate clap;
 extern crate hex;
 extern crate smarket;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fmt;
 use std::fmt::Display;
 
+use smarket::yf::PricingResult;
+
 fn main() -> Result<(), Box<dyn Error>> {
+	let lots: Vec<Lot> = vec![Lot {
+		uid: 1,
+		share_count: ShareCount(100.0),
+		asset_type: AssetType::Usx("TSLA".to_string()),
+		custodian: Custodian("robinhood".to_string()),
+	}];
+	let ladder = Ladder {
+		assets: vec![
+			AssetType::Usx("SPCE".into()),
+			AssetType::Usx("GBTC".into()),
+			AssetType::Usx("PEP".into()),
+			AssetType::Usx("TSLA".into())
+		]
+	};
+
 	let yaml = clap::load_yaml!("cli.yaml");
 	let matches = clap::App::from(yaml).get_matches();
-	if let Some(_matches) = matches.subcommand_matches("lots") {
-		let lots: Vec<Lot> = vec![Lot {
-			uid: 1,
-			share_count: ShareCount(100.0),
-			asset_type: AssetType::Usx("tsla".to_string()),
-			custodian: Custodian("robinhood".to_string()),
-		}];
-		if lots.is_empty() {
-			println!("No lots yet");
-		} else {
-			println!("{:16}  {:15}  {:10}  {}", "LOT ID", "CUSTODIAN", "ASSET", "SHARES");
-			for ref lot in lots {
-				println!("{:16}  {:15}  {:10}  {}", lot.uid_pretty(), lot.custodian.name(), lot.asset_type.name(), lot.share_count)
-			}
-		}
-	} else if let Some(_) = matches.subcommand_matches("assets") {
-		let ladder = Ladder {
-			assets: vec![
-				AssetType::Usx("SPCE".into()),
-				AssetType::Usx("GBTC".into()),
-				AssetType::Usx("PEP".into()),
-				AssetType::Usx("TSLA".into())
-			]
+	if let Some(_) = matches.subcommand_matches("status") {
+		let portions = ladder.portions();
+		let counts = counts(&lots);
+		let price_symbols = {
+			let mut symbols = portions.keys().cloned().collect::<HashSet<_>>();
+			symbols.extend(counts.keys().cloned().collect::<HashSet<_>>());
+			symbols.into_iter().collect::<Vec<_>>()
 		};
-		let rungs = ladder.assets
+		let prices = smarket::yf::price_assets(&price_symbols)?
 			.iter()
-			.enumerate()
-			.map(|(i, asset_type)| {
-				let AssetType::Usx(ref symbol) = asset_type;
-				Rung {
-					symbol: symbol.to_owned(),
-					weight: 1.618f64.powf(i as f64),
-				}
+			.map(|(symbol, result)| {
+				let usd_price = match result {
+					PricingResult::Priced { usd_price, .. } => *usd_price,
+					_ => panic!("missing price")
+				};
+				(symbol.to_string(), usd_price.as_f64())
 			})
-			.collect::<Vec<_>>();
-		let full_weight: f64 = rungs.iter().map(|it| it.weight).sum();
-		let portions = rungs.iter().map(|rung| (rung.symbol.to_owned(), rung.weight / full_weight)).collect::<HashMap<String, _>>();
-		println!("{:10}  {:7}", "ASSET ID", "PORTION");
-		for rung in rungs {
-			let portion = portions.get(&rung.symbol).expect("value");
-			println!("{:10}  {:<5.2}%", rung.symbol, portion * 100.0)
+			.collect::<HashMap<String, _>>();
+		let values = prices.iter().map(|(symbol, price)| {
+			let count = counts.get(symbol).cloned().unwrap_or(0.0);
+			(symbol.to_string(), price * count)
+		}).collect::<HashMap<String, _>>();
+		let full_value: f64 = values.values().sum();
+
+		println!(
+			"{:8}  {:6}    {:10}  {:^6}    {:10}  {:^6}    {:10}  {:^6}",
+			"ASSET ID", "SHARES",
+			"MARKET($)", "%",
+			"TARGET($)", "%",
+			"DRIFT($)", "%"
+		);
+		for symbol in ladder.ordered_symbols() {
+			let target_portion = portions.get(&symbol).expect("portion");
+			let count = counts.get(&symbol).cloned().unwrap_or(0.0);
+			let market = values.get(&symbol).expect("value");
+			let market_portion = market / full_value;
+			let target = target_portion * full_value;
+			let drift = market - target;
+			let drift_portion = market_portion - target_portion;
+			println!(
+				"{:8}  {:6.2}    {:10.1}  {:5.1}%    {:10.1}  {:5.1}%    {:10.1}  {:5.1}% ",
+				symbol, count,
+				market, market_portion * 100.0,
+				target, target_portion * 100.0,
+				drift, drift_portion * 100.0
+			)
 		}
 	} else {
 		eprintln!("No command found");
 	}
 	Ok(())
+}
+
+fn counts(lots: &Vec<Lot>) -> HashMap<String, f64> {
+	let mut map: HashMap<String, f64> = HashMap::new();
+	for lot in lots {
+		let AssetType::Usx(ref symbol) = lot.asset_type;
+		let previous = map.get(symbol).cloned().unwrap_or(0.0);
+		let next = previous + lot.share_count.as_f64();
+		map.insert(symbol.to_string(), next);
+	}
+	map
 }
 
 #[derive(Debug)]
@@ -65,14 +98,34 @@ struct Holding {
 }
 
 #[derive(Debug)]
-struct Rung {
-	pub symbol: String,
-	pub weight: f64,
-}
-
-#[derive(Debug)]
 struct Ladder {
 	pub assets: Vec<AssetType>
+}
+
+impl Ladder {
+	pub fn ordered_symbols(&self) -> Vec<String> {
+		self.assets.iter().map(|it| {
+			let AssetType::Usx(ref symbol) = it;
+			symbol.to_uppercase()
+		}).collect()
+	}
+	pub fn weights(&self) -> HashMap<String, f64> {
+		self.assets
+			.iter()
+			.enumerate()
+			.map(|(i, asset_type)| {
+				let AssetType::Usx(ref symbol) = asset_type;
+				(symbol.to_uppercase(), 1.618f64.powf(i as f64))
+			})
+			.collect::<HashMap<String, _>>()
+	}
+	pub fn portions(&self) -> HashMap<String, f64> {
+		let weights = self.weights();
+		let full_weight: f64 = weights.values().sum();
+		weights.iter()
+			.map(|(symbol, weight)| (symbol.to_string(), *weight / full_weight))
+			.collect::<HashMap<String, _>>()
+	}
 }
 
 #[derive(Clone, Debug)]
@@ -103,6 +156,10 @@ impl Lot {
 
 #[derive(PartialOrd, PartialEq, Copy, Clone, Debug)]
 pub struct ShareCount(f64);
+
+impl ShareCount {
+	pub fn as_f64(&self) -> f64 { self.0 }
+}
 
 impl Display for ShareCount {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
